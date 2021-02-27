a543f6d7b5e5f55f9f5fee2df9d4a2e3a4a2f638
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2021-02-27 09:40:28 +0100

    changement extension

---
+++ pages/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>La Perche Basséenne v1.0 Build 7 - Accueil</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">La Perche Basséenne</a>
+            </div>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Accueil</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-table fa-fw"></i> Formulaire</a>
+                        </li>
+                        <li>
+                            <a href="suivi.html"><i class="fa fa-edit fa-fw"></i> Suivi</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Accueil</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+                    <h2>Bienvenue sur la version web/local du carnet "La Perche Basséenne"</h2><br />
+                    <div class="p-5 mb-5 bg-danger text-white">Pour la saisie des prises, merci de faire un enregistrement pour chaque prise.</div>
+
+                    <h3>CHANGELOG :</h3>
+                    <br />
+                    <ul>
+                        <li>v0.1 : Fichier sous Access : problème de saisie du formulaire</li>
+                        <li>v0.2 : Fichier sous LibreOffice Base de données : impossible à l'ouvrir</li>
+                        <li>v1.0 : Version web en local</li>
+						<li>v1.0 Build 1 : Version web, partie html réalisée uniquement</li>
+						<li>v1.0 Build 4 : page formulaire, suivi mises en forme, refonte de la bdd en sqlite</li>
+						<li>v1.0 Build 5 : Modification du titre des pages + guide d'utilisation rédigé</li>
+                    </ul>
+
+                    <h2>Guide d'utilisation :</h2>
+					<p>En premier lieu, merci de ne pas modifier directement les fichiers.</p>
+					<table>
+						<td><img src="../img/menu.png"></img></td>
+						<td><p>Pour accéder aux pages de saisie et de consultation, veuillez aller dans le menu de gauche. </p></td>
+					</table>
+						<h4>Le formulaire</h4>
+                    <table>
+						<td><img src="../img/jour.png"></img></td>
+						<td>La date est saisie au format automatique.<br />
+						Pour la durée, merci de mettre directement soit le nombre d'heures ou la plage horaire. </td>
+					</table>
+					                    <table>
+						<td><img src="../img/meteo.png"></img></td>
+						<td>Vous devez sélectionner dans le menu déroulante, sauf pour la phase lunaire que vous devez indiquer manuellement. </td>
+					</table><br />
+					                    <table>
+						<td><img src="../img/l'eau.png"></img></td>
+						<td>Étant donné que pour la couleur et la force, il est impossible de faire en menu déroulante, merci de bien indiquer ces deux lignes. </td>
+					</table>
+					                    <table>
+						<td><img src="../img/composition de l'amorce _ materiel et lignes.png"></img></td>
+						<td>Il est possible de saisir en liste (à la ligne), pour agrandir le champs, merci de cliquer sur le coin droit et de le tirer.</td>
+					</table>
+					                    <table>
+						<td><img src="../img/prises.png"></img></td>
+					</table>
+					<p>Pour les prises, le formuaire n'a pas encore testé pour les 11 prises, donc, merci de saisir le formulaire par prise. </p>	
+
+					                    <table>
+						<td><img src="../img/remarques.png"></img></td>
+					<td>Il est possible de saisir en liste (à la ligne), pour agrandir le champs, merci de cliquer sur le coin droit et de le tirer.</td>
+					</table>
+
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="../vendor/raphael/raphael.min.js"></script>
+    <script src="../vendor/morrisjs/morris.min.js"></script>
+    <script src="../data/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
